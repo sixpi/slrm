be0ea60a8322c5133453dcac484718ffbea15200
Bing Xia <aix.bing@gmail.com>
2013-04-29 18:33:11 -0400

    Restructure project using setuptools/distribute

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Metadata
+    name = 'slrm',
+    version = '0.0.1',
+    description = 'Utility for keeping track of all local repositories',
+    author = 'Bing Xia',
+    author_email = "aix.bing@gmail.com",
+    packages = ['slrm'],
+
+    entry_points = {
+        'console_scripts' : ['slrm = slrm.slrm:main']
+    },
+    install_requires=['pygit2'],
+    
+      # Testing
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
--- slrm/slrm.py
+++ slrm/slrm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import project as _p
 import json as _json
